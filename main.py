0000c0302b956ea972c5ef1c83564bdd9ea20509
oaydogdu <91716268+oaydogdu@users.noreply.github.com>
2021-10-03 18:18:57 +0000

    I do not know how to code ' log and power functions '.Thanks.

--- main.py
+++ main.py
@@ -2,8 +2,4 @@ import numpy
 
 number= input( 'Enter number x: ') # '2' 2
 number2= input('Enter number y: ')
-number = float(number)
-
-print(type(number))
-#print(number**number2)
-
+print('0076491')
